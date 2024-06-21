69b0eb7915a92b7097515c1971c23d882a006b90
Ruined-buil <63550843+Ruined-buil@users.noreply.github.com>
2024-06-21 10:37:32 +0700

    Add files via upload

--- Pharmacy_System/src/main/java/Famacy/model/Message.java
+++ Pharmacy_System/src/main/java/Famacy/model/Message.java
@@ -1,24 +1,49 @@
 package Famacy.model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "\"Message\"")
-public class Message {
+public class Message implements Serializable {
 
-    @EmbeddedId
-    private MessageId id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "\"MID\"", updatable = false, nullable = false)
+    private int MID;
 
     @Column(name = "\"content\"")
     private String content;
 
+    @Column(name = "\"receiverID\"")
+    private int receiverID;
+
+    @Column(name = "\"senderID\"")
+    private int senderID;
+
     // Getters and setters
-    public MessageId getId() {
-        return id;
+    public int getMID() {
+        return MID;
     }
 
-    public void setId(MessageId id) {
-        this.id = id;
+    public void setMID(int MID) {
+        this.MID = MID;
+    }
+
+    public int getSenderID() {
+        return senderID;
+    }
+
+    public void setSenderID(int senderID) {
+        this.senderID = senderID;
+    }
+
+    public int getReceiverID() {
+        return receiverID;
+    }
+
+    public void setReceiverID(int receiverID) {
+        this.receiverID = receiverID;
     }
 
     public String getContent() {
