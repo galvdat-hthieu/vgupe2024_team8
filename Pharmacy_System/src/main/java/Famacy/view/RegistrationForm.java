9a0975545938cac4a54147704c6f02a5c0caca98
Orcaa <104363786+Orcaa-or-ak@users.noreply.github.com>
2024-06-29 16:57:24 +0700

    Update RegistrationForm.java

--- Pharmacy_System/src/main/java/Famacy/view/RegistrationForm.java
+++ Pharmacy_System/src/main/java/Famacy/view/RegistrationForm.java
@@ -24,9 +24,10 @@ public class RegistrationForm extends JFrame {
         this.currentUsername = currentUsername;
 
         setTitle("Register Account");
-        setSize(400, 300);
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+        setResizable(false);
 
         registrationPanel = new JPanel();
         add(registrationPanel);
@@ -37,7 +38,7 @@ public class RegistrationForm extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 AccountService accountService = new AccountService();
                 String username = usernameField.getText();
-                String password = new String(passwordField.getPassword());
+                String password = "123";
                 String role = (String) roleComboBox.getSelectedItem(); // Get selected role
                 int employeeId;
                 
@@ -77,36 +78,36 @@ public class RegistrationForm extends JFrame {
         usernameField.setBounds(150, 20, 165, 25);
         panel.add(usernameField);
 
-        JLabel passwordLabel = new JLabel("Password:");
-        passwordLabel.setBounds(10, 50, 80, 25);
-        panel.add(passwordLabel);
+        // JLabel passwordLabel = new JLabel("Password:");
+        // passwordLabel.setBounds(10, 50, 80, 25);
+        // panel.add(passwordLabel);
 
-        passwordField = new JPasswordField(20);
-        passwordField.setBounds(150, 50, 165, 25);
-        panel.add(passwordField);
+        // passwordField = new JPasswordField(20);
+        // passwordField.setBounds(150, 50, 165, 25);
+        // panel.add(passwordField);
 
         JLabel roleLabel = new JLabel("Role:");
-        roleLabel.setBounds(10, 80, 80, 25);
+        roleLabel.setBounds(10, 50, 80, 25);
         panel.add(roleLabel);
 
         roleComboBox = new JComboBox<>(ROLES);
-        roleComboBox.setBounds(150, 80, 165, 25);
+        roleComboBox.setBounds(150, 50, 165, 25);
         panel.add(roleComboBox);
 
         JLabel employeeIdLabel = new JLabel("Employee ID:");
-        employeeIdLabel.setBounds(10, 110, 100, 25);
+        employeeIdLabel.setBounds(10, 80, 100, 25);
         panel.add(employeeIdLabel);
 
         employeeIdField = new JTextField(20);
-        employeeIdField.setBounds(150, 110, 165, 25);
+        employeeIdField.setBounds(150, 80, 165, 25);
         panel.add(employeeIdField);
 
         registerButton = new JButton("Register");
-        registerButton.setBounds(10, 140, 150, 25);
+        registerButton.setBounds(10, 110, 150, 25);
         panel.add(registerButton);
 
         backButton = new JButton("Back");
-        backButton.setBounds(170, 140, 100, 25);
+        backButton.setBounds(170, 110, 100, 25);
         panel.add(backButton);
     }
 
